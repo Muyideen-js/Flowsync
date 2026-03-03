be434f35209f6946245d548c2c377529798071be
Yaomin <149057725+Muyideen-js@users.noreply.github.com>
2026-03-03 22:29:58 +0100

    Import React alongside useState and useEffect

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import TopBar from '../../components/TopBar/TopBar';
